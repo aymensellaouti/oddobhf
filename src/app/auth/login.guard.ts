cb11d84c46b062cdc7a39c3c29e21332976b25cc
aymensellaouti <aymen.sellaouti@gmail.com>
2024-02-22 13:30:45 +0100

    add guard

--- src/app/auth/login.guard.ts
+++ src/app/auth/login.guard.ts
@@ -1,7 +1,17 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from './service/auth.service';
+import { APP_ROUTES } from '../config/routes.const';
 
 
 // si elle retourne true iet3ada sinon ma iet3adach
 export const loginGuard: CanActivateFn = (route, state) => {
+  // Todo : check if user is  logged in
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  if (!authService.isAuthenticated()) {
+    router.navigate([APP_ROUTES.login]);
+    return false;
+  }
   return true;
 };
